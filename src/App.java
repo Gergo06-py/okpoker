fdfc7b32ca21cd08a1feb0b9c16b8a025ce6d645
Gergo06-py <borbelygergo06@gmail.com>
2023-12-05 23:09:25 +0100

    Simple refactor

--- src/App.java
+++ src/App.java
@@ -4,11 +4,17 @@
 * Copyright: 2021, Nagy József 
 * Date: 2021-09-11
 * Licenc: MIT
-*
+* Refactored by: Borbély Gergő Árpád
 */
 
-import controllers.MainController;import views.MainWindow;
-public class App { public static void main(String[] args)
-throws Exception { MainWindow mainWindow = new MainWindow();
-new MainController(mainWindow); mainWindow.setVisible(true);
-}}
+import controllers.MainController;
+import views.MainWindow;
+
+public class App {
+    public static void main(String[] args)
+            throws Exception {
+        MainWindow mainWindow = new MainWindow();
+        new MainController(mainWindow);
+        mainWindow.setVisible(true);
+    }
+}
--- src/views/MainWindow.java
+++ src/views/MainWindow.java
@@ -4,7 +4,7 @@
 * Copyright: 2021, Nagy József 
 * Date: 2021-09-11
 * Licenc: MIT
-*
+* Refactored by: Borbély Gergő Árpád
 */
 package views;
 
@@ -30,14 +30,12 @@ public class MainWindow extends JFrame {
     public JButton turnButton = new JButton();
     public JButton riverButton = new JButton();
 
-
     public MainWindow() {
         this.tablePanel.setSize(100, 100);
         this.tablePanel.setBackground(Color.LIGHT_GRAY);
         this.tablePanel.add(flop1Btn);
         this.tablePanel.add(flop2Btn);
         this.tablePanel.add(flop3Btn);
-        /* A flop-nak nem kell megjelennie flop előtti állapotban */
         this.flop1Btn.setVisible(false);
         this.flop2Btn.setVisible(false);
         this.flop3Btn.setVisible(false);
@@ -51,11 +49,10 @@ public class MainWindow extends JFrame {
         this.tablePanel.add(riverButton);
         this.turnButton.setVisible(false);
         this.riverButton.setVisible(false);
-                
 
         this.setLayout(new BoxLayout(
-            this.getContentPane(), 
-            BoxLayout.PAGE_AXIS));
+                this.getContentPane(),
+                BoxLayout.PAGE_AXIS));
 
         this.add(buttonPanel);
         this.add(tablePanel);
@@ -63,5 +60,5 @@ public class MainWindow extends JFrame {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(300, 250);
     }
-    
+
 }
