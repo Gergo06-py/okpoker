55a3bb341ac238ab94c5e5aa4737ff8fdeadbe2b
Gergo06-py <borbelygergo06@gmail.com>
2023-12-05 22:30:21 +0100

    Starting phase

--- src/views/MainWindow.java
+++ src/views/MainWindow.java
@@ -30,7 +30,6 @@ public class MainWindow extends JFrame {
     public JButton turnButton = new JButton();
     public JButton riverButton = new JButton();
 
-
     public MainWindow() {
         this.tablePanel.setSize(100, 100);
         this.tablePanel.setBackground(Color.LIGHT_GRAY);
@@ -51,11 +50,10 @@ public class MainWindow extends JFrame {
         this.tablePanel.add(riverButton);
         this.turnButton.setVisible(false);
         this.riverButton.setVisible(false);
-                
 
         this.setLayout(new BoxLayout(
-            this.getContentPane(), 
-            BoxLayout.PAGE_AXIS));
+                this.getContentPane(),
+                BoxLayout.PAGE_AXIS));
 
         this.add(buttonPanel);
         this.add(tablePanel);
@@ -63,5 +61,5 @@ public class MainWindow extends JFrame {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(300, 250);
     }
-    
+
 }
